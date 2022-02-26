4fb55070e7eaa34a7a422fc0fd03c5f37c1e48e2
Chinar Bawa <bawachinar111@gmail.com>
2022-02-25 20:18:30 -0500

    Add project files.

---
+++ Models/GreatTables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+namespace GreatTables.Models
+{
+    public class GreatTables
+    {
+        public int Id { get; set; }
+        public string Quality { get; set; }
+
+        public String Color { get; set; }
+        public string Measurements { get; set; }
+        public decimal Price { get; set; }
+        public decimal Rating { get; set; }
+    }
+}
